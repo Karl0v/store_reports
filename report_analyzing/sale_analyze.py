3fb1a5165e3ab85d0cd5d7a6de2406dcafc1b23e
merchant <karlov.ej@gmail.com>
2023-03-27 16:43:00 +0300

    #  в sale_analize.py добавил комментарии

--- report_analyzing/sale_analyze.py
+++ report_analyzing/sale_analyze.py
@@ -3,22 +3,30 @@ from typing import List
 import csv
 
 def sale_analyze(sku_rows: List[SKU]):
+    # создаем пустой словарь, в котором будем хранить все операции для каждого SKU
     sku_dict = dict()
     for row in sku_rows:
+        # если SKU не встречалось раньше, создаем новий список операций
         if row.sku not in sku_dict:
             sku_dict[row.sku] = []
+        # добавляем в найденую операцию для соответствующего SKU в список
         sku_dict[row.sku].append(row)
-
+    # создаем пустой список для отчета о продажах
     sale_report = []
+    # для каждого SKU из словаря sku_dict и соответствующего ему списка операций ...
     for key, value in sku_dict.items():
+        # ... получаем последнюю операция которая была sale
         last_operation = value[-1]
         if last_operation.operation == 'sale':
-            #transportation_cost = 0
             #todo list comprehension позволяет сделать в одну строчку подсчет transportation_cost
+
+            # Создаем список всех трат на транспортировку товара кроме первой и последней операции
             transportation_cost = [operation.operation_cost + 0 for operation in value[1:-1]]
-            """for operation in value[1:-1]:
+            """transportation_cost = 0
+            for operation in value[1:-1]:
                 transportation_cost += operation.operation_cost
             print(transportation_cost))"""
+            # добавляем информацию о продаже и затратах на транспортировку в отчет
             first_operation = value[0]
             report_row = {
                 'SKU': first_operation.sku,
@@ -31,6 +39,7 @@ def sale_analyze(sku_rows: List[SKU]):
             }
             sale_report.append(report_row)
             #todo запись в csv
+    # записывам всю полученую информацию в файл формата csv
     csv_file = 'report_analyzing/sale_analyze_report.csv'
     with open(csv_file, 'w', newline='') as csvfile:
         headers = ['SKU', 'Warehouse', 'Warehouse cell ID', 'Sale date', 'First arrival date', 'Operation_cost',
